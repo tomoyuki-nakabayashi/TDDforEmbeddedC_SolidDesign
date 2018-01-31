8d25cdee1bee2e537bb4673a180f5f7b8d2ce085
Your Name <Your Email>
2018-01-31 12:56:34 +0900

    test: Add LightDriverSpy template

---
+++ test/LightDriverSpy.h
@@ -0,0 +1,25 @@
+#ifndef TEST_LIGHT_DRIVER_SPY_H_
+#define TEST_LIGHT_DRIVER_SPY_H_
+#include "LightDriver.h"
+#include "LightController.h"
+
+LightDriver LightDriverSpy_Create(int id);
+void LightDriverSpy_Destroy(LightDriver);
+void LightDriverSpy_TurnOn(LightDriver);
+void LightDriverSpy_TurnOff(LightDriver);
+
+// Only for spy
+void LightDriverSpy_Reset(void);
+int LightDriverSpy_GetState(int id);
+int LightDriverSpy_GetLastId(void);
+int LightDriverSpy_GetLastState(void);
+void LightDriverSpy_AddSpiesToController(void);
+
+enum {
+  LIGHT_ID_UNKNOWN = -1,
+  LIGHT_STATE_UNKNOWN = -1,
+  LIGHT_OFF = 0,
+  LIGHT_ON = 1
+}
+
+#endif  // TEST_LIGHT_DRIVER_SPY_H_
