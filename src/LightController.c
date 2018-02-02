6a4d28a6a6241e600ae8cf151c3a93392951bfe3
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-02-02 21:17:45 +0900

    Remove switch sentence. Done p. 224

--- src/LightController.c
+++ src/LightController.c
@@ -1,8 +1,6 @@
 #include "LightController.h"
 #include <stdlib.h>
 #include <memory.h>
-#include "X10LightDriver.h"
-#include "LightDriverSpy.h"
 
 static LightDriver lightDrivers[MAX_LIGHTS] = {NULL};
 
@@ -12,30 +10,11 @@ void LightController_Create()
   memset(lightDrivers, 0, sizeof lightDrivers);
 }
 
-static void destroy(LightDriver driver) {
-  if (!driver) return;
-
-  switch (driver->type) {
-    case X10:
-      X10LightDriver_Destroy(driver);
-      break;
-    case AcmeWireless:
-      break;
-    case MemoryMapped:
-      break;
-    case TestLightDriver:
-      LightDriverSpy_Destroy(driver);
-      break;
-    default:
-      break;
-  }
-}
-
 void LightController_Destroy()
 {
   for (int i = 0; i < MAX_LIGHTS; i++) {
     LightDriver driver = lightDrivers[i];
-    destroy(driver);
+    LightDriver_Destroy(driver);
     lightDrivers[i] = NULL;
   }
 }
@@ -45,7 +24,7 @@ bool LightController_Add(int id, LightDriver lightDriver)
   if (id < 0 || id >= MAX_LIGHTS)
     return false;
 
-  destroy(lightDrivers[id]);
+  LightDriver_Destroy(lightDrivers[id]);
 
   lightDrivers[id] = lightDriver;
   return true;
@@ -53,42 +32,10 @@ bool LightController_Add(int id, LightDriver lightDriver)
 
 void LightController_TurnOn(int id)
 {
-  LightDriver driver = lightDrivers[id];
-  if (driver == NULL) return;
-
-  switch (driver->type) {
-    case X10:
-      X10LightDriver_TurnOn(driver);
-      break;
-    case AcmeWireless:
-      break;
-    case MemoryMapped:
-      break;
-    case TestLightDriver:
-      LightDriverSpy_TurnOn(driver);
-      break;
-    default:
-      break;
-  }
+  LightDriver_TurnOn(lightDrivers[id]);
 }
 
 void LightController_TurnOff(int id)
 {
-  LightDriver driver = lightDrivers[id];
-  if (driver == NULL) return;
-
-  switch (driver->type) {
-    case X10:
-      X10LightDriver_TurnOff(driver);
-      break;
-    case AcmeWireless:
-      break;
-    case MemoryMapped:
-      break;
-    case TestLightDriver:
-      LightDriverSpy_TurnOff(driver);
-      break;
-    default:
-      break;
-  }
+  LightDriver_TurnOff(lightDrivers[id]);
 }
